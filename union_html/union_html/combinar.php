c0711c906cd506292975fbc896b80f27e766eaa7
PabloPalRam <92909740+PabloPalRam@users.noreply.github.com>
2023-11-02 10:40:02 +0100

    Update combinar.php

--- union_html/union_html/combinar.php
+++ union_html/union_html/combinar.php
@@ -1,46 +1,30 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
 
-if (isset($_POST['combinar'])) {
-    // Array con los nombres de los archivos HTML que quieres combinar
-    $archivos = ['archivo1.html', 'archivo2.html', 'archivo3.html', 'archivo4.html'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    // Archivo donde se almacenará la combinación
-    $archivoCombinado = 'combinar.html';
+    $headFile = $_FILES["head"]["tmp_name"];
+    $bodyFile = $_FILES["body"]["tmp_name"];
+    $footerFile = $_FILES["footer"]["tmp_name"];
 
-    // Abre el archivo de salida en modo escritura
-    $salida = fopen($archivoCombinado, 'w');
+    $head = file_get_contents($headFile);
+    $body = file_get_contents($bodyFile);
+    $footer = file_get_contents($footerFile);
 
-    // Itera a través de los archivos y escribe su contenido en el archivo combinado
-    foreach ($archivos as $archivo) {
-        $contenido = file_get_contents($archivo);
-        fwrite($salida, $contenido);
-    }
 
-    // Cierra el archivo de salida
-    fclose($salida);
+    $unido= $head."\n".$body."\n".$footer;
+    
 
-    // Muestra el archivo combinado en el navegador
-    header('Content-Type: text/html');
-    readfile($archivoCombinado);
+    $html=nl2br(htmlspecialchars($unido));
+    
+  echo "<script>
+        var combinedTxt = " . json_encode($unido) .";
+        var combinedWindow = window.open('', '_blank');
+        combinedWindow.document.open();
+        combinedWindow.document.write('<pre>' + combinedTxt + '</pre>');
+        combinedWindow.document.close();
+    </script>";
+   
 }
-
-/*
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Combinar archivos HTML</title>
-</head>
-<body>
-    <form action="combinar.php" method="post">
-        <input type="submit" name="combinar" value="Combinar Archivos">
-    </form>
-</body>
-</html>
-*/
-
 ?>
-
 
